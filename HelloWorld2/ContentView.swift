e7608d739a27f007da994d44222a3a5f7e78d60a
Anqi Chen <chenanqi1024@gmail.com>
2026-06-03 21:03:40 +0800

    Initial Commit

---
+++ HelloWorld2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorld2
+//
+//  Created by chenanqi on 2026/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorld2/HelloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld2App.swift
+//  HelloWorld2
+//
+//  Created by chenanqi on 2026/6/3.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
